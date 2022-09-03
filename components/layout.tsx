16d82f72b4f4c966eba3313a163b3bb5c05e6b29
Andy Bitz <artzbitz@gmail.com>
2022-09-03 14:14:57 +0200

    Run prettier and add .editorconfig

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,19 +2,19 @@ import { ReactNode } from 'react';
 import Head from 'next/head';
 
 export function Layout({
-    title,
-    children
+	title,
+	children,
 }: {
-    title?: string;
-    children: ReactNode;
+	title?: string;
+	children: ReactNode;
 }) {
-    return (
-        <main>
-            <Head>
-                <title>{title || 'Katakana Test'}</title>
-                <link rel="icon" type="image/png" href="/ka-icon.png?v=1" />
-            </Head>
-            {children}
-        </main>
-    );
+	return (
+		<main>
+			<Head>
+				<title>{title || 'Katakana Test'}</title>
+				<link rel="icon" type="image/png" href="/ka-icon.png?v=1" />
+			</Head>
+			{children}
+		</main>
+	);
 }
--- components/link.tsx
+++ components/link.tsx
@@ -1,10 +1,17 @@
 import cn from 'classnames';
 import { AnchorHTMLAttributes } from 'react';
 
-export function Link({ className, children, ...props }: AnchorHTMLAttributes<HTMLAnchorElement>) {
-    return (
-        <a className={cn('text-blue-300 hover:underline', className)} {...props}>
-            {children}
-        </a>
-    );
+export function Link({
+	className,
+	children,
+	...props
+}: AnchorHTMLAttributes<HTMLAnchorElement>) {
+	return (
+		<a
+			className={cn('text-blue-300 hover:underline', className)}
+			{...props}
+		>
+			{children}
+		</a>
+	);
 }
